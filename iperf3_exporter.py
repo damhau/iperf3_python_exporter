f90c75e743fc20f19fe9e361d7c1e6c82421594f
Damien Hauser <damien@dhconsulting.ch>
2020-03-05 16:44:45 +0100

    add debug

--- iperf3_exporter.py
+++ iperf3_exporter.py
@@ -64,7 +64,7 @@ def get_data():
     logging.debug("Json data:")
     logging.debug(json_data)
     iperf3_server_port = json_data['port']
-
+    logging.debug("iperf3_server_port:" + str(iperf3_server_port))
     if iperf3_proto == 'tcp':
         client = iperf3.Client()
         if iperf3_duration:
